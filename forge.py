290a43f54f5629469a17081ac1de010745461bff
mohit <mohitmishra309@outlook.com>
2026-04-09 21:59:10 +0530

    Initial Commit

--- forge.py
+++ forge.py
@@ -10,12 +10,10 @@ console = Console()
 
 load_dotenv()
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 llm = init_chat_model(
     model="gpt-4o-mini",
     model_provider="openai",
-    api_key=OPENAI_API_KEY,
     temperature=0.7,
 )
 
